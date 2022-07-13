9bde80e63b658caf3cb717f88faff8f47d82b88d
benedictpandu <beneedictpandu@gmail.com>
2022-07-13 14:16:12 +0700

    Tugas 7

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css'
+
+
+const App = () => {
+  return(
+    <>
+        <div className="card">
+          <h1>Hi, Saya Bene</h1>
+          <hr/> 
+          <p>Saya dari Batch 36 sudah menginstall React </p>
+        </div>
+    </>
+  )
+}
+
+export default App
